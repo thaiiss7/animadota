9f14f91b18840802910aaf3a43e2f0c5aed9b2ac
Leehzitha <leticia.burlinski@gmail.com>
2026-06-19 11:06:26 -0300

    Added hash on password, login useCase

---
+++ Backend/Application/UseCases/User/LoginUseCase.cs
@@ -0,0 +1,30 @@
+using Animadota.Results;
+using Animadota.Services.JWT;
+using Animadota.Services.Users;
+
+public class LoginUseCase (
+    IUserService userService,
+    IJWTService jwtService,
+    IPasswordService passwordService
+)
+{
+    public async Task<Result<LoginResponse>> Do(LoginPayload payload)
+    {
+        var user = await userService.GetUserByUsername(payload.Username);
+
+        if (user is null)
+            return Result<LoginResponse>.Fail("User not found");
+
+        var passwordMatch = passwordService
+            .Compare(payload.Password, user.Senha);
+
+        if(!passwordMatch)
+            return Result<LoginResponse>.Fail("Invalid username or password");
+
+        var jwt = jwtService.GenerateToken(new(
+            user.Id, user.Username
+        ));
+
+        return Result<LoginResponse>.Success(new(jwt));
+    }
+}
