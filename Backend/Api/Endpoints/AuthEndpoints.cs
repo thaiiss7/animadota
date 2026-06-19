dbffa4d23657594cb1b967667640fe28602e7829
Thais Michel <mit1ct@bosch.com>
2026-06-19 16:20:54 -0300

    feat auth/ong endpoint

--- Backend/Api/Endpoints/AuthEndpoints.cs
+++ Backend/Api/Endpoints/AuthEndpoints.cs
@@ -5,9 +5,20 @@ public static class AuthEndpoints
 {
     public static void ConfigureAuthEndpoints(this WebApplication app)
     {
-        app.MapPost("auth", async (
+        app.MapPost("auth/user", async (
             [FromBody]LoginPayload payload,
             [FromServices]LoginUseCase useCase) =>
+        {
+            var result = await useCase.Do(payload);
+            if (!result.IsSuccess)
+                return Results.BadRequest();
+            
+            return Results.Ok(result.Data);
+        });
+
+        app.MapPost("auth/ong", async (
+            [FromBody]OngLoginPayload payload,
+            [FromServices]OngLoginUseCase useCase) =>
         {
             var result = await useCase.Do(payload);
             if (!result.IsSuccess)
